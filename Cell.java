854eedd44548e96193273494302e2802cfdd575f
anishsaraf101 <anishsaraf100@gmail.com>
2025-01-18 20:40:58 -0600

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -2,7 +2,6 @@
     A Maze is made up of Cells
  */
 
-// unedited file given by the instructor
 public class Cell {
     private boolean visited;    // whether the cell has been visited (true if visited, false if not visited)
     private boolean right;      // whether the cell has a right border (true if a right boundary, false if an open right)
